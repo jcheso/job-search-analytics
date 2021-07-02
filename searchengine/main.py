1fa57149bcc690de2934ff3800e017e784266c66
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-07-02 12:16:52 +0930

    Update results layout

--- searchengine/main.py
+++ searchengine/main.py
@@ -6,13 +6,14 @@ from . import job_analytics
 # import job_analytics
 
 # TODO: Create a page that displays the job adverts
+# TODO: Exception control for null searches
 
 
 def get_analytics(job, location):
 
     jobs = job_data.get_job_data(job, location)
     number_of_jobs, average_pay_min, average_pay_max, most_frequent_location, top_companies, currency, top_words, job, location = job_analytics.get_job_analytics(
-    jobs, job, location)
+        jobs, job, location)
 
     return(number_of_jobs, average_pay_min, average_pay_max, most_frequent_location, top_companies, currency, top_words, job, location)
 
